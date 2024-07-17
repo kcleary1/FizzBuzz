5e0aaaa2c5bf0ae2c125d32bbea7971eea5993ed
kcleary1 <kscleary11@gmail.com>
2024-07-17 09:18:52 -0600

    Update after instructor comment

--- public var ThreeFiveNonsense(int x).cs
+++ public var ThreeFiveNonsense(int x).cs
@@ -1,16 +1,14 @@
-
-
 static public string ThreeFiveNonsense(int number)
 
 {
     var answer = "";
 
-    if (number % 3 == 0)
+    if (number % 3 == 0 && number % 5 > 0)
     {
         answer = "fizz";
     }
 
-    if (number % 5 == 0)
+    if (number % 5 == 0 && number % 3 > 0)
     {
         answer = "buzz";
     }
